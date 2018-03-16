c45a61d465fb36baa718e2cd3a7c9352ce8381d1
mongkok <domake.io@gmail.com>
2018-03-17 00:21:07 +0700

    Improved JS selectors

--- graphiql_debug_toolbar/static/graphiql_debug_toolbar/js/parse.js
+++ graphiql_debug_toolbar/static/graphiql_debug_toolbar/js/parse.js
@@ -9,29 +9,33 @@
 
         if (payload && payload.hasOwnProperty('debugToolbar')) {
             var debugToolbar = payload.debugToolbar;
+            var $debugPanelList = $('#djDebugPanelList');
 
             delete payload.debugToolbar;
 
             $.each(debugToolbar.panels, function(id, panel) {
-                var $panel = $(`#${id} .djDebugPanelContent .djdt-scroll`);
-                var $subtitle = $(`#djDebugPanelList input[data-cookie="djdt${id}"]`).next().find('small');;
-
-                $(`#${id} .djDebugPanelTitle h3`).html(panel.title);
-
-                $panel.empty();
-
-                if (!$panel.parent().find('img').length) {
-                    $('<img />', {
-                        src: data.loaderUrl,
-                        alt: 'loading',
-                        class: 'djdt-loader'
-                    }).insertBefore($panel);
-                }
+                var $subtitle = $debugPanelList.find(`input[data-cookie="djdt${id}"]`).next().find('small');
 
                 if ($subtitle.text() != panel.subtitle) {
                     $subtitle.fadeOut(100, function() {
                         $(this).text(panel.subtitle).fadeIn(200);
                     });
+                }
+
+                if (panel.title !== null) {
+                    var $panel = $(`#${id}`);
+                    var $content = $panel.find('.djdt-scroll');
+
+                    $panel.find('h3:first').html(panel.title);
+                    $content.empty();
+
+                    if (!$content.parent().find('img').length) {
+                        $('<img />', {
+                            src: data.loaderUrl,
+                            alt: 'loading',
+                            class: 'djdt-loader'
+                        }).insertBefore($content);
+                    }
                 }
             });
             $('#djDebug').data('store-id', debugToolbar.storeId);
