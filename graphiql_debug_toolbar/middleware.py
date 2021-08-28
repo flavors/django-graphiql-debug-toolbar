25f59c206b2f66c1fe384ed6cc57a8d7d1c0db26
mongkok <domake.io@gmail.com>
2021-08-29 00:29:57 +0700

    Added DebugToolbar.store monkey patch

--- graphiql_debug_toolbar/middleware.py
+++ graphiql_debug_toolbar/middleware.py
@@ -1,37 +1,17 @@
-import functools
 import json
 from collections import OrderedDict
 
 from django.template.loader import render_to_string
 from django.utils.encoding import force_text
 
-from debug_toolbar import middleware as mmod
 from debug_toolbar.middleware import _HTML_TYPES
 from debug_toolbar.middleware import DebugToolbarMiddleware as BaseMiddleware
-from debug_toolbar.middleware import get_show_toolbar
 from debug_toolbar.toolbar import DebugToolbar
 from graphene_django.views import GraphQLView
 
 from .serializers import CallableJSONEncoder
 
 __all__ = ["DebugToolbarMiddleware"]
-
-
-class _DebugToolbar(DebugToolbar):
-    def __init__(self, middleware, *args, **kwargs):
-        middleware._toolbar = self
-        super().__init__(*args, **kwargs)
-
-
-class MockToolbar(object):
-    def __init__(self, middleware):
-        self._middleware = middleware
-
-    def __enter__(self):
-        mmod.DebugToolbar = functools.partial(_DebugToolbar, self._middleware)
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        mmod.DebugToolbar = DebugToolbar
 
 
 def set_content_length(response):
@@ -48,9 +28,6 @@ def get_payload(request, response, toolbar):
         if panel.panel_id == "TemplatesPanel":
             continue
 
-        panel.generate_stats(request, response)
-        panel.generate_server_timing(request, response)
-
         if panel.has_content:
             title = panel.title
         else:
@@ -61,12 +38,18 @@ def get_payload(request, response, toolbar):
             "subtitle": panel.nav_subtitle,
         }
 
-    toolbar.store()
     payload["debugToolbar"]["storeId"] = toolbar.store_id
     return payload
 
 
+_store = DebugToolbar.store
+
+
 class DebugToolbarMiddleware(BaseMiddleware):
+    def __init__(self, get_response):
+        super().__init__(get_response)
+        self.store_id = None
+
     def process_view(self, request, view_func, *args):
         if (
             hasattr(view_func, "view_class")
@@ -75,26 +58,35 @@ class DebugToolbarMiddleware(BaseMiddleware):
         ):
             request._graphiql = True
 
+    def store(self):
+        def decorator(toolbar):
+            _store(toolbar)
+            self.store_id = toolbar.store_id
+
+        return decorator
+
     def __call__(self, request):
-        if not get_show_toolbar()(request) or request.is_ajax():
-            return self.get_response(request)
+        DebugToolbar.store = self.store()
+        response = super().__call__(request)
+        DebugToolbar.store = _store
 
-        with MockToolbar(self):
-            response = super().__call__(request)
         content_type = response.get("Content-Type", "").split(";")[0]
-        html_type = content_type in _HTML_TYPES
-        graphql_view = getattr(request, "_graphql_view", False)
+        is_html = content_type in _HTML_TYPES
+        is_graphiql = getattr(request, "_graphiql", False)
 
-        if response.status_code == 200 and graphql_view and html_type:
+        if is_html and is_graphiql and response.status_code == 200:
             template = render_to_string("graphiql_debug_toolbar/base.html")
             response.write(template)
             set_content_length(response)
 
-        if html_type or not (graphql_view and content_type == "application/json"):
+        if (
+            is_html
+            or self.store_id is None
+            or not (is_graphiql and content_type == "application/json")
+        ):
             return response
 
-        toolbar = self._toolbar
-
+        toolbar = DebugToolbar.fetch(self.store_id)
         payload = get_payload(request, response, toolbar)
         response.content = json.dumps(payload, cls=CallableJSONEncoder)
         set_content_length(response)
